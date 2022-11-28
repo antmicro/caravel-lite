ec49648f49fee7ecb1154c9f344cd5fdedca6a25
Rafal Kolucki <rkolucki@antmicro.com>
2022-11-28 16:21:37 +0100

    Disable PLL and force external clock
    Signed-off-by: Rafal Kolucki <rkolucki@antmicro.com>

--- verilog/rtl/caravel.v
+++ verilog/rtl/caravel.v
@@ -750,10 +750,10 @@ module caravel (
 		.VPWR(vccd_core),
 		.VGND(vssd_core),
     `endif
-        .ext_clk_sel(ext_clk_sel),
+        .ext_clk_sel(1'b1),
         .ext_clk(clock_core_buf),
-        .pll_clk(pll_clk),
-        .pll_clk90(pll_clk90),
+        .pll_clk(1'b0),
+        .pll_clk90(1'b0),
         .resetb(rstb_l_buf),
         .sel(spi_pll_sel),
         .sel2(spi_pll90_sel),
@@ -765,19 +765,19 @@ module caravel (
 
     // DCO/Digital Locked Loop
 
-    digital_pll pll (
-    `ifdef USE_POWER_PINS
-		.VPWR(vccd_core),
-		.VGND(vssd_core),
-    `endif
-        .resetb(rstb_l_buf),
-        .enable(spi_pll_ena),
-        .osc(clock_core_buf),
-        .clockp({pll_clk, pll_clk90}),
-        .div(spi_pll_div),
-        .dco(spi_pll_dco_ena),
-        .ext_trim(spi_pll_trim)
-    );
+    // digital_pll pll (
+    // `ifdef USE_POWER_PINS
+	// 	.VPWR(vccd_core),
+	// 	.VGND(vssd_core),
+    // `endif
+    //     .resetb(rstb_l_buf),
+    //     .enable(spi_pll_ena),
+    //     .osc(clock_core_buf),
+    //     .clockp({pll_clk, pll_clk90}),
+    //     .div(spi_pll_div),
+    //     .dco(spi_pll_dco_ena),
+    //     .ext_trim(spi_pll_trim)
+    // );
 
     // Housekeeping interface
 
